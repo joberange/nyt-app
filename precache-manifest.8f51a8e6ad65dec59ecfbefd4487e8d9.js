b75d4113074a9d31f20c4ebcdb8fe5d263dd1a30
Jobe Range <rangejobe@gmail.com>
2018-11-03 13:40:19 -0700

    Updates

---
+++ precache-manifest.8f51a8e6ad65dec59ecfbefd4487e8d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/nyt-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "1150f26b740720bc13c7",
+    "url": "/nyt-app/static/js/runtime~main.1150f26b.js"
+  },
+  {
+    "revision": "e04f0dfb5bddc7fe4d34",
+    "url": "/nyt-app/static/js/main.e04f0dfb.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/nyt-app/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "e04f0dfb5bddc7fe4d34",
+    "url": "/nyt-app/static/css/main.4cff7dac.chunk.css"
+  },
+  {
+    "revision": "81efe70413c3c85dc720436eb559fd84",
+    "url": "/nyt-app/index.html"
+  }
+];
